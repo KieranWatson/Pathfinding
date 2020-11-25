e5e1c771930dba9afc426c12ab8001af01b3ac18
Kieran Watson <61968335+KieranWatson@users.noreply.github.com>
2020-11-25 11:53:06 +0000

    Added a player and made enemy follw it

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    //Gets the animator component to activate the animation
+    public Animator Ani;
+
+    //Sets bool to false.
+    bool Throw = false;
+
+    // Update is called once per frame.
+    void Update()
+    {
+        //Activates the animation on the press of the W key.
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Throw = true;
+            Ani.SetBool("throw", true);
+        }
+
+        //Stops the animation when you let go of the key and the aniamtion ends.
+        else if (Input.GetKeyUp(KeyCode.T))
+        {
+            Throw = false;
+            Ani.SetBool("throw", false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float movementSpeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControllPlayer();
+    }
+
+    void ControllPlayer()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+         if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        }
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+    }
+}
